9f311873475479497e33709be826dc47917d3ee3
Ross A. Baker <ross@rossabaker.com>
2023-12-13 12:09:06 -0500

    Fix type of SRV target (#1)
    * Fix type of SRV target
    
    * SRV target is not nullable
    
    * Add SRV record to tests
    
    ---------
    
    Co-authored-by: Janik <80165193+Janik-Haag@users.noreply.github.com>

--- utils/tests/domains.nix
+++ utils/tests/domains.nix
@@ -128,6 +128,9 @@
         "example.org" = {
           cname = { data = [ "www.example.com" ]; ttl = 60; };
         };
+        "_xmpp._tcp.example.org" = {
+          srv = { data = [{ port = 5223; priority = 10; weight = 5; target = "host1.example.com."; }]; ttl = 60; };
+        };
       };
     };
   };
