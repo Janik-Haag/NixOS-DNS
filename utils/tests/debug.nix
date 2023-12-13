bdd855d7e9677b95719cb23eb46f82a5548c3083
Ross A. Baker <ross@rossabaker.com>
2023-12-13 08:31:23 -0500

    Format

--- utils/tests/debug.nix
+++ utils/tests/debug.nix
@@ -47,7 +47,7 @@ in
           cname = { data = [ "www.example.com" ]; ttl = 60; };
         };
         "_xmpp._tcp.example.org" = {
-          srv = { data = [ { port = 5223; priority = 10; weight = 5; target = "host1.example.com."; } ]; ttl = 60; };
+          srv = { data = [{ port = 5223; priority = 10; weight = 5; target = "host1.example.com."; }]; ttl = 60; };
         };
       };
     };
--- utils/tests/domains.nix
+++ utils/tests/domains.nix
@@ -129,7 +129,7 @@
           cname = { data = [ "www.example.com" ]; ttl = 60; };
         };
         "_xmpp._tcp.example.org" = {
-          srv = { data = [ { port = 5223; priority = 10; weight = 5; target = "host1.example.com."; } ]; ttl = 60; };
+          srv = { data = [{ port = 5223; priority = 10; weight = 5; target = "host1.example.com."; }]; ttl = 60; };
         };
       };
     };
