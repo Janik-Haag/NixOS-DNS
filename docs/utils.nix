398ccd24439b015a75ed40cf24c6b689989c0885
Janik H. <janik@aq0.de>
2024-04-17 15:03:36 +0200

    cleanup: nixdoc patch landed in nixpkgs

--- docs/utils.nix
+++ docs/utils.nix
@@ -14,14 +14,6 @@ let
       "extend"
     ]
   );
-  patchedNixdoc = nixdoc.overrideAttrs (o: {
-    patches = (o.patches or [ ]) ++ [
-      (fetchpatch {
-        url = "https://github.com/nix-community/nixdoc/commit/b4480a2143464d8238402514dd35c78b6f9b9928.patch";
-        hash = "sha256-WZ/tA2q+u4h7G1gUn2nkAutsjYHNxqXwjqAKpxYTf7k=";
-      })
-    ];
-  });
 in
 runCommand "utils" { } ''
   mkdir -p $out
@@ -29,7 +21,7 @@ runCommand "utils" { } ''
   ${lib.concatLines (
     builtins.map (
       name:
-      "${lib.getExe' patchedNixdoc "nixdoc"} --file ${../utils/${name}.nix} --prefix 'utils' --category '${name}' --description '${name}' > $out/${name}.md"
+      "${lib.getExe' nixdoc "nixdoc"} --file ${../utils/${name}.nix} --prefix 'utils' --category '${name}' --description '${name}' > $out/${name}.md"
     ) utilNames
   )}
 ''
