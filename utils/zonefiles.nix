7b7197014f82f6384f4e818e16e0dd74a252c2df
Janik H. <janik@aq0.de>
2024-05-03 15:56:58 +0200

    fix: txt records for too long values

--- utils/zonefiles.nix
+++ utils/zonefiles.nix
@@ -1,6 +1,41 @@
 { lib, utils }:
 {
   /*
+    Converts a string into a valid txt record so it's compliant with RFC 4408
+    This means it splits the string every 255 chars and surrounds it with quotation marks
+
+    Type:
+      utils.zonefiles.formatTxtRecord :: String -> String
+  */
+  formatTxtRecord =
+    # The String of a txt resource record
+    txtString:
+    let
+      format =
+        {
+          acc ? [ ],
+          chars,
+        }:
+        let
+          rest = [ (lib.concatStrings (lib.take 255 chars)) ];
+        in
+        if (lib.length chars) > 255 then
+          format {
+            acc = acc ++ rest;
+            chars = lib.drop 255 chars;
+          }
+        else if acc != [ ] then
+          acc ++ rest
+        else
+          rest;
+      resolve = lib.concatStringsSep "\" \"" (format {
+        chars = lib.stringToCharacters txtString;
+      });
+    in
+    "\"${resolve}\"";
+
+  /*
+    attributeset
     Takes any record from the module and converts it to a fitting zonefile string
 
     Type:
@@ -24,10 +59,17 @@
     else if record == "soa" then
       "SOA ${value.mname}. ${value.rname}. ( ${builtins.toString value.serial} ${builtins.toString value.refresh} ${builtins.toString value.retry} ${builtins.toString value.expire} ${builtins.toString value.ttl} )"
     else if record == "txt" then
-      "TXT \"${value}\""
+      "TXT ${utils.zonefiles.formatTxtRecord value}"
     else
       "${lib.toUpper record} ${value}";
+  /*
+    Converts a zone attributeset into a zonefile and returns a multiline string
+
+    Type:
+      utils.zonefiles.mkZoneString :: Attr -> String
+  */
   mkZoneString =
+    # Takes dnsConfig."your-domain.invalid"
     entries:
     ''${lib.concatLines (
       lib.flatten (
